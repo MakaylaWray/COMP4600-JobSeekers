a79a39a725167ff429c511715d2107264f952b69
Makayla Wray <makaylawray@makaylas-mbp.mynetworksettings.com>
2026-03-29 12:16:27 -0400

    Add subpages and build main nav bar

---
+++ js/active-page.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var navBar = document.getElementsByClassName('main-nav-bar')[0];
+    if (!navBar) {
+        return;
+    }
+
+    var links = navBar.getElementsByTagName('a');
+    var currentPath = window.location.pathname.split('/').pop() || 'index.html';
+
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].closest('.button')) {
+            continue;
+        }
+
+        var href = links[i].getAttribute('href');
+        if (!href) {
+            continue;
+        }
+
+        var linkPath = new URL(href, window.location.href).pathname.split('/').pop();
+        if (linkPath === currentPath) {
+            links[i].classList.add('current-page-link');
+        }
+    }
+};
